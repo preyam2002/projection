96b4531c7e7a1635ac2348b3eb529fc952d10fc7
Preyam Rao <preyam2002@gmail.com>
2025-11-06 01:51:26 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "pattern.css/dist/pattern.min.css";
 import "./globals.css";
 
@@ -48,6 +49,7 @@ export default function RootLayout({
             </a>
           </footer>
         </div>
+        <Analytics />
       </body>
     </html>
   );
